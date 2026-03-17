c9a27fe502a364a14e2eb1ea9ab02c373f0742b5
684244150 <684244150@mail.pbru.ac.th>
2026-03-17 11:15:39 +0700

    Add files via upload

---
+++ Lab/Lab4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  
+  ofstream MyWriteFile("filename.txt");
+
+
+  MyWriteFile << "Files can be tricky, but it is fun enough-1";
+  MyWriteFile << "Files can be tricky, but it is fun enough-2";
+ 
+  
+  MyWriteFile.close();
+
+ 
+  string myText;
+
+ 
+  ifstream MyReadFile("filename.txt");
+
+  
+  while (getline (MyReadFile, myText)) {
+   
+    cout << myText;
+  }
+
+  
+  MyReadFile.close();
+}
